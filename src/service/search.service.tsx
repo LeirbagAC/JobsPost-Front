00b646dc8dbc1ca212cb2f52ac0ba74a6a86a4b6
LeirbagAC <albuquerqueg373@gmail.com>
2026-06-25 15:23:26 -0300

    Fix: encodeURIComponent no sistema de busca

--- src/service/search.service.tsx
+++ src/service/search.service.tsx
@@ -3,7 +3,7 @@ import { JobPost } from "@/types";
 
 export const search = async(query: string): Promise<JobPost[]> => {
     try {
-        const response = await api.get(`/JobPost/search?keyword=${query}`);
+        const response = await api.get(`/JobPost/search?keyword=${encodeURIComponent(query)}`);
         return response.data;
     } catch (err) {
         console.error('Erro ao buscar vagas: ', err);
